bcd3aec31daa8ee395a83819aec846b5a8b0e6d9
Tom Spurling <tom@tspurling.co.uk>
2025-11-24 10:09:12 +0000

    getting_started_ollama: More debug logging for requests

--- day-1/getting_started_ollama.py
+++ day-1/getting_started_ollama.py
@@ -1,4 +1,17 @@
 import requests
+import logging
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
+
+try: # for Python 3
+    from http.client import HTTPConnection
+except ImportError:
+    from httplib import HTTPConnection
+HTTPConnection.debuglevel = 1
 
 response = requests.post(
     "http://localhost:11434/api/generate",
